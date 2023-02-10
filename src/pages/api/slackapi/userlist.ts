91ea09192a9b9b779192feb5e713404d6caf6664
udonkot <udonko104develop@gmail.com>
2023-02-11 00:08:45 +0900

    [add]slackAPI実装、ユーザ一覧、チャンネル一覧の表示画面作成

--- src/pages/api/slackapi/userlist.ts
+++ src/pages/api/slackapi/userlist.ts
@@ -43,10 +43,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     })
 
-    res.status(200).json({
-      result: 'ok',
-      userlist:userList
-    });
+    res.status(200).json(userList);
 
   } catch (error) {
     console.error(error)
